dad2e36e5c2442dec81821762866311540cb2ba7
Karenina Wongsordjo <karenina2798@gmail.com>
2024-02-26 16:28:34 +0900

    adding to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :goals, presence: true
+  has_many :submissions
+  has_many :goals_as_owner, through: :submissions, source: :goals
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
