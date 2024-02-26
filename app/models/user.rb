f38eaae68b196ef7cde2d887438423480eca291c
Go <92448616+gosuz@users.noreply.github.com>
2024-02-26 17:10:03 +0900

    Update user.rb changed from goal_as_owner to goal_as_user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :goals
   has_many :submissions
-  has_many :goals_as_owner, through: :submissions, source: :goals
+  has_many :goals_as_user, through: :submissions, source: :goals
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
         #  test
