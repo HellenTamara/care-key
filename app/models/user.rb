0b592c853e11c43622588cc1512d0a882b58b6fa
Karenina Wongsordjo <155918488+AKW9@users.noreply.github.com>
2024-02-26 16:44:09 +0900

    Update user.rb
    delete presence

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :goals, presence: true
+  has_many :goals
   has_many :submissions
   has_many :goals_as_owner, through: :submissions, source: :goals
   devise :database_authenticatable, :registerable,
