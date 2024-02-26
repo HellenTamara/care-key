dc0020ef68a9cfce7eb639535c70bf181d3ec172
Go Suzuki <go.suz55@gmail.com>
2024-02-26 20:32:28 +0900

    started seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,16 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Creating main users..."
+go = User.new(email: "go.suz55@gmail.com", password: "123456", name: "Go Suzuki")
+go.save
+
+nina = User.new(email: "karenina2798@gmail.com", password: "123456", name: "Nina Wongsordjo")
+nina.save
+
+sarah = User.new(email: "sealove14@aim.com", password: "123456", name: "Sarah Amour")
+sarah.save
+
+hellen = User.new(email: "hellen.tamara@outlook.com", password: "123456", name: "Hellen Naito")
+hellen.save
