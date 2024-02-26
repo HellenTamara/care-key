494ae5c6e4bb4178f4d8f6883cb79c0c399a56bb
Karenina Wongsordjo <155918488+AKW9@users.noreply.github.com>
2024-02-26 17:55:08 +0900

    Update user.rb
    added validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,5 @@ class User < ApplicationRecord
   belongs_to :skin_color, class_name: "Part", optional: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :name, :email, :password, presence: true
 end
