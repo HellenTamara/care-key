7bca5b758fa34ed3ef96eb682c0749161cf99a0b
Karenina Wongsordjo <karenina2798@gmail.com>
2024-02-27 11:33:52 +0900

    finished the routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,15 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  # get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  get "/home", to: "home#pages", as: :home
+  get "/avatar/edit", to: "edit#pages", as: :edit_avatar
+  patch "/avatar", to: "update#pages"
+  resources :goals, only: [:new, :create, :index] do
+    resources :submissions, only: [:create]
+  end
 end
