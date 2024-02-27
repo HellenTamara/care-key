94c2e072437063dc4538bdb043db79bc5954e1dc
Hellen Tamara <hellen.tamara@outlook.com>
2024-02-27 18:29:30 +0900

    setting the default parts for the user

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,13 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
   def home
-    @user = current_user
+    if current_user
+      current_user.hair = Part.where(category: "hair").first if current_user.hair.nil?
+      current_user.shirt = Part.where(category: "shirt").first if current_user.shirt.nil?
+      current_user.shoes = Part.where(category: "shoes").first if current_user.shoes.nil?
+      current_user.skin_color = Part.where(category: "skin_color").first if current_user.skin_color.nil?
+      current_user.pants = Part.where(category: "pants").first if current_user.pants.nil?
+      current_user.save
+    end
   end
 end
--- app/models/part.rb
+++ app/models/part.rb
@@ -1,3 +1,4 @@
 class Part < ApplicationRecord
+  has_many :users
   has_one_attached :photo
 end
