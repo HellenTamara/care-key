071f9299b07c64b1b4111a4a508be9cf897c87ce
Hellen Tamara <hellen.tamara@outlook.com>
2024-02-27 16:08:18 +0900

    setting up cloudinary

--- app/models/part.rb
+++ app/models/part.rb
@@ -1,2 +1,3 @@
 class Part < ApplicationRecord
+  has_one_attached :photo
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-    
-ActiveRecord::Schema[7.1].define(version: 2024_02_26_083504) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_27_070538) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum"
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
 
   create_table "goals", force: :cascade do |t|
     t.string "name"
@@ -32,6 +59,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_26_083504) do
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "submissions", force: :cascade do |t|
+    t.date "date"
+    t.bigint "goal_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["goal_id"], name: "index_submissions_on_goal_id"
+    t.index ["user_id"], name: "index_submissions_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -58,7 +95,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_26_083504) do
     t.index ["skin_color_id"], name: "index_users_on_skin_color_id"
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "goals", "users"
+  add_foreign_key "submissions", "goals"
+  add_foreign_key "submissions", "users"
   add_foreign_key "users", "parts", column: "hair_id"
   add_foreign_key "users", "parts", column: "pants_id"
   add_foreign_key "users", "parts", column: "shirt_id"
