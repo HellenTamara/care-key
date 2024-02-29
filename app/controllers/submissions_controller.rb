0dc007fd232cc5ed79c56aad715ddcce2cdbcb8a
Go <92448616+gosuz@users.noreply.github.com>
2024-02-29 17:44:55 +0900

    Update submissions_controller.rb
    Removed some things we didn't need, and added the notice pop up just in case the submission doesn't go through

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -8,38 +8,23 @@ class SubmissionsController < ApplicationController
     @submissions = Submission.all
     @submission = @submissions.first
 
-    # code from create
-
-
     # Its going to need the goal instance in order for it to submit for the correct goal
     # Find the id of the goal itself??? Can't be params[:id] cause its not exactly from a form. Its form a previous form
     # @submission.goal = Goal.new()
     # How is it going to know its the exact goal you're trying to submit??
     # need to give it a date from the goal itself
-    submission = Submission.new(params[:goal_id])
-    @date = Goal.find(params["goal_id"].date_id)
-    @goal = Goal.find(params["goal_id"])
-    submission.goal = @goal
-    submission.user = current_user
-    submission.save
-    raise
   end
 
-  # Copy paste this and delete this before pushing (remember you're still on the created-submission-forms-branch)
   def create
     submission = Submission.new(submission_params) # needs to be getting date through some params i think -> Ideally want it to be getting it through the seeded data. Submission.start_date????
-
-    # uncomment here later
     @goal = Goal.find(params["goal_id"])
     submission.goal= @goal
     submission.user = current_user
     if submission.save
       # where do I want to redirect to? home path??
-      redirect_to home_path()
+      redirect_to home_path(), notice: "Well done!"
     else
-      raise
-      # render the form???
-      # render "shelters/show", status: :unprocessable_entity
+      redirect_to home_path(), notice: "Sorry we're having issues"
     end
   end
 
