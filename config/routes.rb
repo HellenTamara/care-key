702f210469b745ab84b646a6a2b5d873b09d9008
Sarah Amour <samour621@gmail.com>
2024-03-01 12:02:55 +0900

    redirecting to homepage updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#landing"
+  root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   # root "posts#index"
 
 
-  get "/home", to: "pages#home", as: :home
+  # get "/home", to: "pages#home", as: :home
   get "/goals/dashboard", to: "goals#dashboard", as: :dashboard
 
 
