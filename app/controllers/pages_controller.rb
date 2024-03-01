14dcda20bbec1915c0191d66c57b238364ea3745
Hellen Tamara <hellen.tamara@outlook.com>
2024-03-01 11:41:30 +0900

    creating the goals for when a user creates an account

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,18 +3,19 @@ class PagesController < ApplicationController
   def home
     @user = current_user
 
-    @sleep_goal = @user.goals.where(name: "Sleep").last
-    @food_goal = @user.goals.where(name: "Food").last
-    @exercise_goal = @user.goals.where(name: "Exercise").last
-
-    @sleep_frequency = @sleep_goal.frequency
-    @food_frequency = @food_goal.frequency
-    @exercise_frequency = @exercise_goal.frequency
+    @goal_sleep = current_user.current_sleep_goal
+    @goal_exercise = current_user.current_exercise_goal
+    @goal_eating = current_user.current_eating_goal
 
 
-    @sleep_achieved_amount = @sleep_goal.submissions.where(achieved: true)
-    @food_achieved_amount = @food_goal.submissions.where(achieved: true)
-    @exercise_achieved_amount = @exercise_goal.submissions.where(achieved: true)
+    @sleep_frequency = @goal_sleep.frequency
+    @food_frequency = @goal_eating.frequency
+    @exercise_frequency = @goal_exercise.frequency
+
+
+    @sleep_achieved_amount = @goal_sleep.submissions.where(achieved: true)
+    @food_achieved_amount = @goal_eating.submissions.where(achieved: true)
+    @exercise_achieved_amount = @goal_exercise.submissions.where(achieved: true)
 
     @sleep_percentage = (@sleep_achieved_amount.count * 100) / @sleep_frequency
     @food_percentage = (@food_achieved_amount.count * 100)  / @food_frequency
