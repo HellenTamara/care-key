a868d88079046aeeb0f70aa7597fd6fc21e92240
Karenina Wongsordjo <karenina2798@gmail.com>
2024-03-01 14:23:00 +0900

    add missing end in pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,27 +1,28 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [:home]
+
   def home
     if user_signed_in?
       @user = current_user
 
-    @goal_sleep = current_user.current_sleep_goal
-    @goal_exercise = current_user.current_exercise_goal
-    @goal_eating = current_user.current_eating_goal
+      @goal_sleep = current_user.current_sleep_goal
+      @goal_exercise = current_user.current_exercise_goal
+      @goal_eating = current_user.current_eating_goal
 
-    @sleep_frequency = @goal_sleep.frequency
-    @food_frequency = @goal_eating.frequency
-    @exercise_frequency = @goal_exercise.frequency
+      @sleep_frequency = @goal_sleep.frequency
+      @food_frequency = @goal_eating.frequency
+      @exercise_frequency = @goal_exercise.frequency
 
+      @sleep_achieved_amount = @goal_sleep.submissions.where(achieved: true)
+      @food_achieved_amount = @goal_eating.submissions.where(achieved: true)
+      @exercise_achieved_amount = @goal_exercise.submissions.where(achieved: true)
 
-    @sleep_achieved_amount = @goal_sleep.submissions.where(achieved: true)
-    @food_achieved_amount = @goal_eating.submissions.where(achieved: true)
-    @exercise_achieved_amount = @goal_exercise.submissions.where(achieved: true)
+      @sleep_percentage = (@sleep_achieved_amount.count * 100) / @sleep_frequency
+      @food_percentage = (@food_achieved_amount.count * 100) / @food_frequency
+      @exercise_percentage = (@exercise_achieved_amount.count * 100) / @exercise_frequency
 
-    @sleep_percentage = (@sleep_achieved_amount.count * 100) / @sleep_frequency
-    @food_percentage = (@food_achieved_amount.count * 100)  / @food_frequency
-    @exercise_percentage = (@exercise_achieved_amount.count * 100)  / @exercise_frequency
-
-    @hp_bar_level = (@sleep_percentage + @food_percentage + @exercise_percentage)
+      @hp_bar_level = (@sleep_percentage + @food_percentage + @exercise_percentage)
+    end
   end
 
   # def landing
