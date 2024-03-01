a9975ceed829022995b7dade766f4ee950311292
Karenina Wongsordjo <karenina2798@gmail.com>
2024-03-01 14:31:31 +0900

    pull lastest master & edited submissions controller

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -5,7 +5,6 @@ class SubmissionsController < ApplicationController
 
     # code from create
 
-
     # Its going to need the goal instance in order for it to submit for the correct goal
     # Find the id of the goal itself??? Can't be params[:id] cause its not exactly from a form. Its form a previous form
     # @submission.goal = Goal.new()
@@ -22,42 +21,45 @@ class SubmissionsController < ApplicationController
 
   # Copy paste this and delete this before pushing (remember you're still on the created-submission-forms-branch)
   def create
-    submission = Submission.new(submission_params) # needs to be getting date through some params i think -> Ideally want it to be getting it through the seeded data. Submission.start_date????
+    @submissions = Submission.new(submission_params) # needs to be getting date through some params i think -> Ideally want it to be getting it through the seeded data. Submission.start_date????
 
     # uncomment here later
     @goal = Goal.find(params["goal_id"])
-    submission.goal= @goal
-    submission.user = current_user
-    if submission.save
+    @submission.goal = @goal
+    @submissions.user = current_user
+    @coins = current_user.avatar.coins
+    if @submission.achieved && @submissions.save
       # where do I want to redirect to? home path??
+      @coins += 50
       redirect_to home_path(), notice: "Well done!"
     else
-      redirect_to home_path(), notice: "Sorry we're having issues"
+      render :index, status: :unprocessable_entity, notice: "Sorry we're having issues"
       # render the form???
       # render "shelters/show", status: :unprocessable_entity
     end
   end
 
   private
+
   def submission_params
     params.require(:submission).permit(:achieved, :date)
   end
 
-  def create
-    @submissions = Submission.new(submission_params)
-    @submissions.user = current_user
-    @coins = current_user.avatar.coins
-    if @submission.achieved && @submissions.save
-      @coins += 50
-      redirect_to home_path()
-    else
-      render :index, status: :unprocessable_entity
-    end
-  end
+  # def create
+  #   @submissions = Submission.new(submission_params)
+  #   @submissions.user = current_user
+  #   @coins = current_user.avatar.coins
+  #   if @submission.achieved && @submissions.save
+  #     @coins += 50
+  #     redirect_to home_path()
+  #   else
+  #     render :index, status: :unprocessable_entity
+  #   end
+  # end
 
-  private
+  # private
 
-  def submission_params
-    params.require(:submission).permit(:date, :achieved, :duration)
-  end
+  # def submission_params
+  #   params.require(:submission).permit(:date, :achieved, :duration)
+  # end
 end
