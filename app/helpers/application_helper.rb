2f4bda826adc598f14d35ec9d92697205cd978c2
Sarah Amour <samour621@gmail.com>
2024-03-04 18:16:43 +0900

    dashboard calendar and homepage layout updated

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,31 @@
 module ApplicationHelper
+  def calendar_food_color(date)
+    if current_user.food_percentage_for(date) < 40
+      "color: #FF6073;"
+    elsif current_user.food_percentage_for(date)< 80
+      "color: #EFB622"
+    else
+      "color: #008C9B"
+    end
+  end
+
+  def calendar_sleep_color(date)
+    if current_user.sleep_percentage_for(date) < 40
+      "color: #FF6073;"
+    elsif current_user.sleep_percentage_for(date)< 80
+      "color: #EFB622"
+    else
+      "color: #008C9B"
+    end
+  end
+
+  def calendar_exercise_color(date)
+    if current_user.weekly_exercise_percentage_for(date) < 40
+      "color: #FF6073;"
+    elsif current_user.weekly_exercise_percentage_for(date) < 80
+      "color: #EFB622"
+    else
+      "color: #008C9B"
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ApplicationRecord
   def create_avatar
     avatar = Avatar.create(user: self)
     avatar.hair = Part.where(category: "hair").first
-    avatar.shoes= Part.where(category: "shoes").first
+    avatar.shoes = Part.where(category: "shoes").first
     avatar.pants = Part.where(category: "pants").first
     avatar.shirt = Part.where(category: "shirt").first
     avatar.skin_color = Part.where(category: "skin_color").first
@@ -21,14 +21,50 @@ class User < ApplicationRecord
   end
 
   def current_sleep_goal
-    goals.where(name: "Sleep").find_by("start_date <=? AND end_date >= ?", Date.today, Date.today) || Goal.create(name: 'Sleep', start_date: Date.today, end_date: Date.today + 6, frequency: 7, user: self, duration: 8)
+    sleep_goal_for(Date.today) || Goal.create(name: 'Sleep', start_date: Date.today, end_date: Date.today + 6, frequency: 7, user: self, duration: 8)
+  end
+
+  def sleep_goal_for(date)
+    goals.where(name: "Sleep").find_by("start_date <= :date AND end_date >= :date", date: date)
   end
 
   def current_exercise_goal
-    goals.where(name: "Exercise").find_by("start_date <=? AND end_date >= ?", Date.today, Date.today) || Goal.create(name: 'Exercise', start_date: Date.today, end_date: Date.today + 6, frequency: 3, user: self)
+    exercise_goal_for(Date.today) || Goal.create(name: 'Exercise', start_date: Date.today, end_date: Date.today + 6, frequency: 3, user: self)
   end
 
   def current_eating_goal
-    goals.where(name: "Food").find_by("start_date <=? AND end_date >= ?", Date.today, Date.today) || Goal.create(name: 'Food', start_date: Date.today, end_date: Date.today + 6, frequency: 21, user: self)
+    food_goal_for(Date.today) || Goal.create(name: 'Food', start_date: Date.today, end_date: Date.today + 6, frequency: 21, user: self)
   end
+
+  def food_goal_for(date)
+    goals.where(name: "Food").find_by("start_date <= :date AND end_date >= :date", date: date)
+  end
+
+  def exercise_goal_for(date)
+    goals.where(name: "Exercise").find_by("start_date <= :date AND end_date >= :date", date: date)
+  end
+
+  def sleep_percentage_for(date)
+    goal = sleep_goal_for(date)
+    return 0 unless goal
+    (goal.submissions.where(["achieved = ? and date = ?", true, date]).count * 100) / (goal.frequency / 7) #this is based on daily goals
+  end
+
+  def food_percentage_for(date)
+    goal = food_goal_for(date)
+    return 0 unless goal
+    (goal.submissions.where(["achieved = ? and date = ?", true, date]).count * 100) / (goal.frequency / 7) #this is based on daily goals
+  end
+
+  def weekly_exercise_percentage_for(date)
+    goal = exercise_goal_for(date)
+    return 0 unless goal
+    goal.submissions.where(achieved: true).count / goal.frequency.to_f * 100
+  end
+
+  def daily_exercise_percentage_for(date)
+    goal = exercise_goal_for(date)
+    goal.submissions.where(["achieved = ? and date = ?", true, date]).count * 100
+  end
+
 end
